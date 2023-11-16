4be0a4067615a1da9de0a1463d74f472f02d5253
Mahmodul Hasan <nicesanto92@gmail.com>
2023-11-16 19:09:07 +0600

    Add useDebouncedCallback hook to handleSearch function

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,20 +2,22 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
 export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const router = useRouter();
-  const handleSearch = (e: string) => {
+  const handleSearch = useDebouncedCallback((e: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set("page", "1");
     if (e) {
       params.set("query", e);
     } else {
       params.delete("query");
     }
     router.replace(`${pathname}?${params.toString()}`);
-  };
+  }, 300);
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
