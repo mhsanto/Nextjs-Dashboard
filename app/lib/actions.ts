2d3498c3d242327cb34f429340dc4157ae977a08
Mahmodul Hasan <nicesanto92@gmail.com>
2023-11-18 21:40:55 +0600

    implemented Next auth user can login logout

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import { signIn } from "@/auth";
 export type State = {
   errors?: {
     customerId?: string[];
@@ -93,12 +94,25 @@ export async function updateInvoice(id: string, formData: FormData) {
   redirect("/dashboard/invoices");
 }
 export async function deleteInvoice(id: string) {
-  throw new Error("test");
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath("/dashboard/invoices");
   } catch (error: any) {
     console.log(error.message);
+  }
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', Object.fromEntries(formData));
+  } catch (error) {
+    if ((error as Error).message.includes('CredentialsSignin')) {
+      return 'CredentialsSignin';
+    }
+    throw error;
   }
 }
